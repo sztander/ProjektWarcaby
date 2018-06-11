0cc05bd0f9c3ca5f0a28031e538e46554dd0b4fd
sztander <sztander14@wp.pl>
2018-06-11 19:37:29 +0200

    Stworzenie SLN

---
+++ Logika/Logika.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Logika
+{
+    public class Logika
+    {
+        Thread gracz1;
+        Thread grazc2;
+        Szachownica szachownica;
+        Szachownica szachownicaGracza1;
+        Szachownica szachownicaGracza2;
+        public void Start()
+        {
+            gracz1 = new Thread(szachownicaGracza1.Start());
+            szachownica = new Szachownica();
+        }
+    }
+}
---
+++ Logika/Szachownica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logika
+{
+    class Szachownica
+    {
+        /// <summary>
+        /// 0 to nie ma nic, 1 to biale 2 to czarne
+        /// </summary>
+        public int[,] tabela = new int[8, 8] { { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 2, 0, 2, 0, 2, 0, 2, 0 }, { 0, 2, 0, 2, 0, 2, 0, 2 }, { 2, 0, 2, 0, 2, 0, 2, 0 } };
+        public void Start()
+        {
+            
+        }
+    }
+}
